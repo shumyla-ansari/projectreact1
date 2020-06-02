7d83c5a597f648524e6ce8453fa5d1df794a617d
Shumyla Ansari <shumyla.fahd@hotmail.com>
2020-06-02 12:23:09 +0100

    changed app.js file

--- src/App.js
+++ src/App.js
@@ -8,8 +8,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Welcome to my first React Base App.
         </p>
+        <p>Shumyla Ansari</p>
         <a
           className="App-link"
           href="https://reactjs.org"
